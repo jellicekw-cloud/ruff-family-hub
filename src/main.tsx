3ef5e7c8e8556312acfa2e4bd03aa82599876b6a
jellicekw-cloud <jellicekw@gmail.com>
2026-08-14 13:57:45 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { registerServiceWorker } from './utils/pushNotifications';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 // Register the service worker early so it's ready before anyone tries to
 // enable chore reminder notifications.
@@ -10,7 +11,9 @@ registerServiceWorker();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );
 
